97c75692a721180be10dadaabf7d340af75a1897
David García I Tejada <alumno.859908@ies-azarquiel.com>
2025-09-22 17:04:09 +0200

    Ejercicios clase 22-09-2025

---
+++ miPrimerProyecto/src/primerPrograma/Ejercicios.java
@@ -0,0 +1,16 @@
+package primerPrograma;
+
+public class Ejercicios {
+	public static void main(String[] args) {
+		int x;
+		int y;
+		int z;
+		final float PI=3.141592F;
+		final float E=2.718281F;
+		x=1;
+		y=4;
+		z=10;
+		
+		System.out.println(2 * x + 0.5F * y - 1/5.0 * z);
+	}
+}
--- miPrimerProyecto/src/primerPrograma/TiposDeDatos.java
+++ miPrimerProyecto/src/primerPrograma/TiposDeDatos.java
@@ -5,10 +5,10 @@ public class TiposDeDatos {
 	public static void main(String[] args) {
 		
 		// Vamos a probar los tipos de datos boolean
-		boolean hayToner;
+//		boolean hayToner;
 		
-		hayToner = true;
-		hayToner = false;
+//		hayToner = true;
+//		hayToner = false;
 		
 		// tipos caracter
 		String frase; // variable donde guardar un texto largo ""
@@ -22,7 +22,9 @@ public class TiposDeDatos {
 		frase = "Hola\na todos";
 		System.out.println(frase);
 		
+		
 		// Tipos númericos enteros
+		
 		byte pequeña;
 		short mediana;
 		int grande;
@@ -34,6 +36,30 @@ public class TiposDeDatos {
 		enorme = 2918378564535L;
 		
 		
+		// Tipos númericos decimales
+		
+		float pocosDecimales;
+		double muchosDecimales;
+		
+		pocosDecimales = 179819847.0958937F;
+		muchosDecimales = 87.9875938745632897;
+		
+		boolean resultado = mediana > grande;
+		System.out.println(resultado);
+		
+		
+		
+		// probar los operadores lógicos
+		boolean hayToner = true;
+		boolean hayPapel = false;
+		resultado = hayToner && hayPapel;
+		System.out.println("Puedo imprimir?: " + resultado);
+		
+		
+		boolean familiaNumerosa = true;
+		boolean rentaFamiliaBaja = false;
+		resultado = familiaNumerosa || rentaFamiliaBaja;
+		System.out.println("¿Me darán beca?: " + resultado);
+		
 	}
-
 }
