53aba4cabb0cf231aa94e02408bc3b0306758edd
David Garcia I Tejada <alumno.859908@ies-azarquiel.com>
2025-10-09 14:05:20 +0200

    EjerciciosAlternativas -> Terminados

--- instruccionAlternativa/src/ejerciciosInstrucciones/Ejercicio11.java
+++ instruccionAlternativa/src/ejerciciosInstrucciones/Ejercicio11.java
@@ -12,8 +12,111 @@ public class Ejercicio11 {
 	public static void main(String[] args) {
 		
 		Scanner teclado = new Scanner(System.in);
-		int num;
+		int num,decenas,unidades;
+		String textoDecenas = "",textoUnidades = "";
 		
+		System.out.print("Introduce un número entre 0 - 99: ");
+		num = teclado.nextInt();
+		teclado.close();
+		
+		if (num < 0 || num > 99) {
+			System.out.println("ERROR: El número introducido no es válido.");
+		} else {
+			
+			decenas = num / 10;
+			unidades = num % 10;
+			
+			switch (decenas) {
+			case 1:
+				if(unidades == 0) {
+					textoDecenas = "Diez";
+				}else if(unidades == 1) {
+					textoDecenas = "Once";
+				}else if(unidades == 2) {
+					textoDecenas = "Doce";
+				}else if(unidades == 3) {
+					textoDecenas = "Trece";
+				}else if(unidades == 4) {
+					textoDecenas = "Catorce";
+				}else if(unidades == 5) {
+					textoDecenas = "Quince";
+				}else {
+					textoDecenas = "Dieci";
+				}
+				break;
+			case 2:
+				if(unidades == 0) {
+					textoDecenas = "Veinte";
+				}else {
+					textoDecenas = "Veinti";
+				}
+				break;
+			case 3:
+				textoDecenas = "Treinta";
+				break;
+			case 4:
+				textoDecenas = "Cuarenta";
+				break;
+			case 5:
+				textoDecenas = "Cincuenta";
+				break;
+			case 6:
+				textoDecenas = "Sesenta";
+				break;
+			case 7:
+				textoDecenas = "Setenta";
+				break;
+			case 8:
+				textoDecenas = "Ochenta";
+				break;
+			case 9:
+				textoDecenas = "Noventa";
+				break;
+			}
+			
+			switch (unidades) {
+			case 0:
+				textoUnidades = "";
+				break;
+			case 1:
+				textoUnidades = "Uno";
+				break;
+			case 2:
+				textoUnidades = "Dos";
+				break;
+			case 3:
+				textoUnidades = "Tres";
+				break;
+			case 4:
+				textoUnidades = "Cuatro";
+				break;
+			case 5:
+				textoUnidades = "Cinco";
+				break;
+			case 6:
+				textoUnidades = "Seis";
+				break;
+			case 7:
+				textoUnidades = "Siete";
+				break;
+			case 8:
+				textoUnidades = "Ocho";
+				break;
+			case 9:
+				textoUnidades = "Nueve";
+				break;
+			}
+			if (decenas == 0) {
+				System.out.printf(textoUnidades);
+			} else if(decenas == 1 && unidades >= 1 && decenas == 1 && unidades <= 5) {
+				System.out.printf(textoDecenas);
+			} else if(decenas == 1 || decenas == 2){
+				System.out.printf("%s%s",textoDecenas,textoUnidades.toLowerCase());
+			}else {
+				System.out.printf("%s y %s",textoDecenas,textoUnidades.toLowerCase());
+			}
+			
+		}		
 
 	}
 
