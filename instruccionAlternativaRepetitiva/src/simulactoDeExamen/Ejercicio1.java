6ece6eedebd50d1980624bc0657192cc23c104ea
David Garcia I Tejada <alumno.859908@ies-azarquiel.com>
2025-11-13 11:57:55 +0100

    Update

--- instruccionAlternativaRepetitiva/src/simulactoDeExamen/Ejercicio1.java
+++ instruccionAlternativaRepetitiva/src/simulactoDeExamen/Ejercicio1.java
@@ -5,18 +5,16 @@ import java.util.Scanner;
 public class Ejercicio1 {
 	public static void main(String[] args) {
 		Scanner teclado = new Scanner(System.in);
-		int n, decenas,unidades;
-		boolean iguales = false;
+		int n;
 		
-		do {
-			System.out.print("Teclea un número: ");
+		System.out.println("Telcea un número: ");
+		n = teclado.nextInt();
+		
+		for (int i = 0; n % 11 != 0 || (n>= 99 && n<=0);) {
+			System.out.println("Telcea un número: ");
 			n = teclado.nextInt();
-			decenas = n / 10;
-			unidades = n % 10;
-			if(decenas == unidades) {
-				iguales = true;
-			}
-		} while (iguales == false && (n <= 99 && n >= 0));
+		}
+		
 		System.out.println("El programa finalizó con éxito");
 		teclado.close();
 	}
