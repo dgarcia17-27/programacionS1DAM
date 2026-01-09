2376d19a6711b7a1efa0dea5c7ce46aa8bb34146
David García <alumno.859908@ies-azarquiel.es>
2026-01-09 11:50:05 +0100

    update

---
+++ EjerciciosStringBuilder/src/Ejercicio9.java
@@ -0,0 +1,8 @@
+public class Ejercicio9 {
+    public static void main(String[] args) {
+        System.out.println(contarPalabra("la","La calle de la plaza"));
+    }
+    public static int contarPalabra(String palabra, String frase) {
+
+    }
+}
