3b719d993ebda5044802100753e1b754d942c07a
David Garcia I Tejada <alumno.859908@ies-azarquiel.com>
2026-01-21 09:43:36 +0100

    Update

--- Examen3-GarciaTejada-David/src/Ejercicio1.java
+++ Examen3-GarciaTejada-David/src/Ejercicio1.java
@@ -4,25 +4,20 @@ public class Ejercicio1 {
         double prodTotal = 0;
 
         for (int i = 1; i <= 100; i++) {
-            switch (i){
-                case 1,40:
-                    cantidadHoras = objetivo / (cantidadCoches * i);
-                    prodTotal = cantidadHoras * ((cantidadCoches * i) * 1);
-                    break;
-                case 41,80:
-                    cantidadHoras = objetivo / (cantidadCoches * i);
-                    prodTotal = cantidadHoras * ((cantidadCoches * i) * 0.9);
-                    break;
-                case 81,95:
-                    cantidadHoras = objetivo / (cantidadCoches * i);
-                    prodTotal = cantidadHoras * ((cantidadCoches * i) * 0.8);
-                    break;
-                case 96,100:
-                    cantidadHoras = objetivo / (cantidadCoches * i);
-                    prodTotal = cantidadHoras * ((cantidadCoches * i) * 0.77);
-                    break;
+            if (i <= 40){
+                cantidadHoras = objetivo / (cantidadCoches * i);
+                prodTotal = cantidadHoras * ((cantidadCoches * i) * 1);
+            } else if(i <= 80){
+                cantidadHoras = objetivo / (cantidadCoches * i);
+                prodTotal = cantidadHoras * ((cantidadCoches * i) * .9);
+            } else if (i <= 95) {
+                cantidadHoras = objetivo / (cantidadCoches * i);
+                prodTotal = cantidadHoras * ((cantidadCoches * i) * .8);
+            } else {
+                cantidadHoras = objetivo / (cantidadCoches * i);
+                prodTotal = cantidadHoras * ((cantidadCoches * i) * .77);
             }
-            System.out.printf("Velocidad: %d Horas: %d Total: %f\n",i,cantidadHoras,prodTotal);
+            System.out.printf("Velocidad: %d Horas: %d Total: %.2f\n",i,cantidadHoras,prodTotal);
         }
     }
 }
