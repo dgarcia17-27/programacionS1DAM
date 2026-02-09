5b17ec669372773279f0fb0c3a55d9b793558383
David <dvigt.12@gmail.com>
2026-02-09 23:55:03 +0100

    Update

--- ejercicios2Eval/src/Ejercicio4.java
+++ ejercicios2Eval/src/Ejercicio4.java
@@ -10,25 +10,38 @@ public class Ejercicio4 {
 
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        int seg, min, horas;
 
-        System.out.print("Teclea las horas: ");
-        horas = teclado.nextInt();
-        System.out.print("Teclea los minutos: ");
-        min = teclado.nextInt();
-        System.out.print("Teclea los segundos: ");
-        seg = teclado.nextInt();
-        teclado.close();
+        System.out.println("Teclea una hora compuesta de: H:M:S : ");
+        StringBuilder hora = new StringBuilder(teclado.nextLine() + ':');
+        String aux = "";
+        int seg = 0, min = 0, horas = 0;
 
-        min += horas * 60;
+        for (int i = 0; i < hora.length(); i++) {
+            if (hora.charAt(i) != ':'){
+                aux += hora.charAt(i); // Voy agregando cada apartado de la hora completa
+            } else {
+                if (horas == 0){ // compruebo que están vacías y las voy rellenaando de forma escalonada
+                    horas = Integer.parseInt(aux); // Las convierto a enteros
+                } else if (min == 0) {
+                    min = Integer.parseInt(aux);
+                } else {
+                    seg = Integer.parseInt(aux);
+                }
+                aux = ""; // voy vaciando la variable en cada vuelta
+            }
+        }
+        // paso todo a segundos
+        seg += horas * 3600;
         seg += min * 60;
 
+        // Convierto todo
         horas = seg / 3600;
         seg %= 3600;
         min = seg / 60;
         seg %= 60;
 
-        System.out.printf("%d:%d:%d",horas,min,seg);
+        // Imprimo la hora correcta
+        System.out.printf("%02d:%02d:%02d",horas,min,seg);
     }
 
 }
