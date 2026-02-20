5cb10932528912ee414bbfd472d93aa2ec4f9174
David <dvigt.12@gmail.com>
2026-02-20 19:47:46 +0100

    Update

--- ejercicios2Eval-GarciaTejada-David/Ejercicio2_2.java
+++ ejercicios2Eval-GarciaTejada-David/Ejercicio2_2.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 public class Ejercicio2_2 {
 
     /**
-     ¿Alguna vez has visto palabras como camelCasing o miVariableSuperLarga? Este estilo de escritura se llama
-     ”Camel Case” y es muy común en programación. Las palabras se escriben juntas, sin espacios, y cada palabra
-     después de la primera empieza con mayúscula.
-     Tu misión es crear una función que rompa el Camel Case, insertando un espacio antes de cada letra mayúscula
-     que encuentre dentro de la palabra. EJEMPLOS:
-     camelCasing = ”camel” + ”Casing”
-     miVariableLarga = ”mi” + ”Variable” + ”Larga”
+         ¿Alguna vez has visto palabras como camelCasing o miVariableSuperLarga? Este estilo de escritura se llama
+         ”Camel Case” y es muy común en programación. Las palabras se escriben juntas, sin espacios, y cada palabra
+         después de la primera empieza con mayúscula.
+         Tu misión es crear una función que rompa el Camel Case, insertando un espacio antes de cada letra mayúscula
+         que encuentre dentro de la palabra. EJEMPLOS:
+         camelCasing = ”camel” + ”Casing”
+         miVariableLarga = ”mi” + ”Variable” + ”Larga”
      */
 
     public static void main(String[] args) {
--- ejercicios2Eval-GarciaTejada-David/Ejercicio7.java
+++ ejercicios2Eval-GarciaTejada-David/Ejercicio7.java
@@ -1,25 +1,22 @@
 public class Ejercicio7 {
     public static void main(String[] args) {
         int [][] sudoku = {
-        {7,8,4, 1,5,9, 3,2,6},
-        {5,3,9, 6,7,2, 8,4,1},
-        {6,1,2, 4,3,8, 7,5,9},
-        {9,2,8, 7,1,5, 4,6,3},
-        {3,5,7, 8,4,6, 1,9,2},
-        {4,6,1, 9,2,3, 5,8,7},
-        {8,7,6, 3,9,4, 2,1,5},
-        {2,4,3, 5,6,1, 9,7,8},
-        {1,9,5, 2,8,7, 6,3,4}
+            {7,8,4, 1,5,9, 3,2,6},
+            {5,3,9, 6,7,2, 8,4,1},
+            {6,1,2, 4,3,8, 7,5,9},
+            {9,2,8, 7,1,5, 4,6,3},
+            {3,5,7, 8,4,6, 1,9,2},
+            {4,6,1, 9,2,3, 5,8,7},
+            {8,7,6, 3,9,4, 2,1,5},
+            {2,4,3, 5,6,1, 9,7,8},
+            {1,9,5, 2,8,7, 6,3,4}
         };
-        int filas = sudoku.length; //filas
-        int columnas = sudoku[0].length; //columnas
+        int filas = sudoku.length;
+        int columnas = sudoku[0].length;
         int numIncorrecto;
-        //dimensión
-        //comprobar si es cuadrada, mayor que 0, y raíz cuadrada numero entero
         if (filas == columnas && filas>0 && Math.sqrt(filas) == (int)Math.sqrt(filas)){
-            //comparar las filas y las columnas, para comprobar si hay algún número repetido
             for (int i = 0; i < filas; i++) {
-                for (int j = 0; j < sudoku[i].length; j++) { //podría usarse columnas
+                for (int j = 0; j < sudoku[i].length; j++) {
                     for (int k = 0; k < sudoku[i].length; k++) {
                         if (j == k){
                             break;
@@ -35,8 +32,6 @@ public class Ejercicio7 {
                     }
                 }
             }
-            
-            //recorrer secciones de 3x3
             int raiz = (int) Math.sqrt(sudoku.length);
             for (int i = 0; i < sudoku.length; i+= raiz){
                 for (int j = 0; j < sudoku[i].length; j+= raiz){
@@ -45,10 +40,10 @@ public class Ejercicio7 {
                             for (int m = i; m < i + raiz; m++){
                                 for (int n = j; n < j + raiz; n++) {
                                     if (sudoku[k][l] == sudoku[m][n]) {
-                                        if (l == n || k == m) {
+                                        if (l == n && k == m) {
                                             continue;
                                         }
-                                        System.out.printf("Hay un valor repetido en un 3x3. Valor repetido: %d %n", sudoku[k][l]);
+                                        System.out.print("Hay un valor repetido en un 3x3.");
                                         break;
                                     }
                                 }
